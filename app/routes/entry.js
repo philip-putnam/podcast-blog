7ad1e5ce25ac0a0c70b28b6e6e920d499a6e7d61
Dan Lauby and Philip Putnam <dan@>
2017-03-23 09:43:30 -0700

    add comment model, component, and display comments to entry routes

--- app/routes/entry.js
+++ app/routes/entry.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('post', params.post_id);
-  }
+    return Ember.RSVP.hash({
+      post: this.store.findRecord('post', params.post_id),
+      comment: this.store.findAll('comment')
+    });
+  },
 });
