f1e5ac21243bd17251bc94e193d9333a9a2d444f
Dan Lauby and Philip Putnam <dan@>
2017-03-23 14:48:30 -0700

    add carousel cat and styling

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,29 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
   },
+}); Ember.Controller.extend({
+    breakpoints: [
+        {
+          'breakpoint': 1024,
+          'settings': {
+            'slidesToShow': 3,
+            'slidesToScroll': 3,
+            'infinite': true
+          }
+        },
+        {
+          'breakpoint': 600,
+          'settings': {
+            'slidesToShow': 2,
+            'slidesToScroll': 2
+          }
+        },
+        {
+          'breakpoint': 480,
+          'settings': {
+            'slidesToShow': 1,
+            'slidesToScroll': 1
+          }
+        }
+    ]
 });
